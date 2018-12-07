cc37da8b18be86d581fe257e0d2156ebc49ac718
Дмитрий Лукин <lukin.dmitriy@gmail.com>
2018-12-07 23:28:46 +0500

    Fix ADC для определения напряжения USB и Батареек

--- src/handlers/EnergyHandler.cpp
+++ src/handlers/EnergyHandler.cpp
@@ -1,9 +1,12 @@
 #ifndef ENERGY_HANDLER
 #define ENERGY_HANDLER
 
-#include "esp_adc_cal.h"
-
-static esp_adc_cal_characteristics_t *adc_chars;
+#define ADCMAX				4095 //Max value at ADC_WIDTH_BIT_12
+#define ADCMIN				0
+#define VOLTAGEMAX			3900 //Reference voltage at ADC_ATTEN_11db
+#define VOLTAGEMIN  		0
+#define ADC_TO_VOLTAGE( ADCx )  ( VOLTAGEMIN + (VOLTAGEMAX-VOLTAGEMIN)*( (ADCx)-ADCMIN )/(ADCMAX-ADCMIN) )
+#define VOLTAGE_BEFORE_DIVIDER( VOLTAGE, R1, R2 )   ( VOLTAGE ) * ( (R1) + (R2) ) / (R2)
 
 class EnergyPeriodicHandler {
 	public:
@@ -13,14 +16,13 @@ class EnergyPeriodicHandler {
 		static bool IsInited;
 };
 
+
 bool EnergyPeriodicHandler::IsInited = false;
 
 void EnergyPeriodicHandler::Init() {
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_11db);
     adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_11db);
-
-    adc_chars = (esp_adc_cal_characteristics_t*)calloc(1, sizeof(esp_adc_cal_characteristics_t));
 
     IsInited = true;
 }
@@ -30,12 +32,13 @@ void EnergyPeriodicHandler::Pool() {
 		Init();
 
 	if (Time::Uptime() %5 == 0) {
-	    uint16_t USBValue	= (uint16_t)adc1_get_raw(ADC1_CHANNEL_5);
-	    uint16_t BATValue	= (uint16_t)adc1_get_raw(ADC1_CHANNEL_4);
+	    uint16_t USBValueSrc	= (uint16_t)adc1_get_raw(ADC1_CHANNEL_5);
+	    uint16_t BATValueSrc	= (uint16_t)adc1_get_raw(ADC1_CHANNEL_4);
 
-	    uint32_t voltage = ::esp_adc_cal_raw_to_voltage(BATValue, adc_chars);
+		uint16_t USBValue 		= VOLTAGE_BEFORE_DIVIDER( ADC_TO_VOLTAGE(USBValueSrc), 51, 100 );
+		uint16_t BATValue 		= VOLTAGE_BEFORE_DIVIDER( ADC_TO_VOLTAGE(BATValueSrc), 100, 51 );
 
-		if (USBValue > 3072)
+		if (USBValue > 5000)
 			Device.PowerMode = DevicePowerMode::CONST;
 		else
 			Device.PowerMode = DevicePowerMode::BATTERY;
