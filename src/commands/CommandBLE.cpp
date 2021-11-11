854a9a278e46a3a3ce9e6ced0a10f1c2efefe760
Дмитрий Лукин <lukin.dmitriy@gmail.com>
2021-11-11 19:29:41 +0500

    v2.40 возможность повтора BLE сигнала

--- src/commands/CommandBLE.cpp
+++ src/commands/CommandBLE.cpp
@@ -14,6 +14,8 @@
 extern DataEndpoint_t 	*Data;
 extern BLEServer_t		BLEServer;
 
+static string CommandBLELastKBDSignalSended = "";
+
 class CommandBLE_t : public Command_t {
 	public:
 		CommandBLE_t() {
@@ -23,6 +25,8 @@ class CommandBLE_t : public Command_t {
 			Events["kbd_key"]			= 0x01;
 			Events["kbd_keydown"]		= 0x02;
 			Events["kbd_keyup"]			= 0x03;
+			Events["kbd_key_repeat"]	= 0x04;
+
 		}
 
 		bool Execute(uint8_t EventCode, const char* StringOperand) override {
@@ -70,9 +74,23 @@ class CommandBLE_t : public Command_t {
 					Result = BLEServer.Write(int(StringOperand[0]));
 				}
 
-				Log::Add((Result > 0) ? Log::Events::Commands::BLEExecuted : Log::Events::Commands::BLEFailed);
+				if (Result > 0)
+				{
+					Log::Add(Log::Events::Commands::BLEExecuted);
+					CommandBLELastKBDSignalSended = StringOperand;
+				}
+				else
+					Log::Add(Log::Events::Commands::BLEFailed);
 
-				return true;
+				return (Result > 0);
+			}
+
+			if (EventCode == 0x04)
+			{
+				if (CommandBLELastKBDSignalSended != "")
+					return Execute(0x01, CommandBLELastKBDSignalSended.c_str());
+
+				return false;
 			}
 
 			return false;
