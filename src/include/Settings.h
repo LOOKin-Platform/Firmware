3978ddc80a721606ed866626f8fbade4870b27f1
Дмитрий Лукин <lukin.dmitriy@gmail.com>
2022-01-12 14:55:49 +0500

    v2.41 - migrate to esp-idf 4.3.2

--- src/include/Settings.h
+++ src/include/Settings.h
@@ -38,7 +38,7 @@ typedef struct FirmwareVersionStruct {
 
 class Settings_t {
 	public:
-		FirmwareVersion 					Firmware = FirmwareVersion(2, 40, 638);
+		FirmwareVersion 					Firmware = FirmwareVersion(2, 41, 0001);
 
 //		const FirmwareVersion Firmware =  0x020A0000;
 
