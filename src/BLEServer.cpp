7975dac80efe155f2c5ad273b08bb18d5000a023
Дмитрий Лукин <lukin.dmitriy@gmail.com>
2022-02-16 23:41:02 +0500

    Updated NimBLE with HID/BASIC fix on start

--- src/BLEServer.cpp
+++ src/BLEServer.cpp
@@ -39,19 +39,19 @@ static const uint8_t _hidReportDescriptor[] = {
   LOGICAL_MAXIMUM(1),	0x01,			//   Logical Maximum (1)
   REPORT_SIZE(1),		0x01,			//   REPORT_SIZE (1)
   REPORT_COUNT(1),		0x08,			//   REPORT_COUNT (8)
-  INPUT(1),				0x02,			//   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  HIDINPUT(1),			0x02,			//   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
   REPORT_COUNT(1),		0x01,			//   REPORT_COUNT (1) ; 1 byte (Reserved)
   REPORT_SIZE(1),		0x08,			//   REPORT_SIZE (8)
-  INPUT(1),				0x01,			//   INPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  HIDINPUT(1),			0x01,			//   INPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
   REPORT_COUNT(1),		0x05,			//   REPORT_COUNT (5) ; 5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
   REPORT_SIZE(1),		0x01,			//   REPORT_SIZE (1)
   USAGE_PAGE(1),		0x08,			//   USAGE_PAGE (LEDs)
   USAGE_MINIMUM(1),		0x01,			//   USAGE_MINIMUM (0x01) ; Num Lock
   USAGE_MAXIMUM(1),		0x05,			//   USAGE_MAXIMUM (0x05) ; Kana
-  OUTPUT(1),			0x02,			//   OUTPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  HIDOUTPUT(1),			0x02,			//   OUTPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
   REPORT_COUNT(1),		0x01,			//   REPORT_COUNT (1) ; 3 bits (Padding)
   REPORT_SIZE(1),		0x03,			//   REPORT_SIZE (3)
-  OUTPUT(1),			0x01,			//   OUTPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  HIDOUTPUT(1),			0x01,			//   OUTPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
   REPORT_COUNT(1),		0x06,			//   REPORT_COUNT (6) ; 6 bytes (Keys)
   REPORT_SIZE(1),		0x08,			//   REPORT_SIZE(8)
   LOGICAL_MINIMUM(1),	0x00,			//   LOGICAL_MINIMUM(0)
@@ -59,7 +59,7 @@ static const uint8_t _hidReportDescriptor[] = {
   USAGE_PAGE(1),		0x07,			//   USAGE_PAGE (Kbrd/Keypad)
   USAGE_MINIMUM(1),		0x00,			//   USAGE_MINIMUM (0)
   USAGE_MAXIMUM(1),		0x65,			//   USAGE_MAXIMUM (0x65)
-  INPUT(1),				0x00,			//   INPUT (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  HIDINPUT(1),			0x00,			//   INPUT (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0),                 	// END_COLLECTION
   // ------------------------------------------------- Media Keys
   USAGE_PAGE(1),		0x0C,			// 		USAGE_PAGE (Consumer)
@@ -106,7 +106,7 @@ static const uint8_t _hidReportDescriptor[] = {
   USAGE(2),				0x83, 0x01,		//   Usage (Media sel)   ; bit 6: 64
   USAGE(2),				0x8A, 0x01,		//   Usage (Mail)        ; bit 7: 128
 */
-  INPUT(1),				0x02,			//   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  HIDINPUT(1),			0x02,			//   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
   END_COLLECTION(0)						// END_COLLECTION
 };
 
@@ -260,7 +260,7 @@ void BLEServer_t::StartAdvertisingAsHID()
 
 	CurrentMode = HID;
 
-	ForceHIDMode(BASIC);
+	//ForceHIDMode(BASIC);
 	ESP_LOGD(Tag, "Advertising started!");
 
 	isRunning = true;
