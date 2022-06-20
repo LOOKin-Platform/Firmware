c81b9995f803ba0200f3921d8faf36b2738bf2bc
Dmitriy Lukin <lukin.dmitriy@gmail.com>
2022-06-20 12:09:12 +0500

    Plug improvements

--- src/commands/CommandSwitch.cpp
+++ src/commands/CommandSwitch.cpp
@@ -23,28 +23,28 @@ class CommandSwitch_t : public Command_t {
     }
 
     bool Execute(uint8_t EventCode, const char* StringOperand) override {
-    		bool Executed = false;
+    	bool Executed = false;
 
-    		if (EventCode == 0x00)
-    			return false;
-
-    		if (EventCode == 0x01) {
-				GPIO::Write(Settings.GPIOData.GetCurrent().Switch.GPIO, true);
-    			Executed = true;
-    		}
-
-    		if (EventCode == 0x02) {
-    			GPIO::Write(Settings.GPIOData.GetCurrent().Switch.GPIO, false);
-    			Executed = true;
-    		}
-
-    		if (Executed) {
-    			//ESP_LOGI(tag, "Executed. Event code: %s, Operand: %s", Converter::ToHexString(EventCode, 2).c_str(), Converter::ToHexString(Operand, 8).c_str());
-        		if (Sensor_t::GetSensorByID(ID + 0x80) != nullptr)
-					Sensor_t::GetSensorByID(ID + 0x80)->Update();
-    			return true;
-    		}
-
+    	if (EventCode == 0x00)
     		return false;
+
+    	if (EventCode == 0x01) {
+			GPIO::Write(Settings.GPIOData.GetCurrent().Switch.GPIO, true);
+    		Executed = true;
+    	}
+
+    	if (EventCode == 0x02) {
+    		GPIO::Write(Settings.GPIOData.GetCurrent().Switch.GPIO, false);
+    		Executed = true;
+    	}
+
+    	if (Executed) {
+    		//ESP_LOGI(tag, "Executed. Event code: %s, Operand: %s", Converter::ToHexString(EventCode, 2).c_str(), Converter::ToHexString(Operand, 8).c_str());
+        	if (Sensor_t::GetSensorByID(ID + 0x80) != nullptr)
+				Sensor_t::GetSensorByID(ID + 0x80)->Update();
+    		return true;
+    	}
+
+    	return false;
     }
 };
