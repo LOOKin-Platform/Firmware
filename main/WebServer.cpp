dcfa8210329923df63a11a672d5fa929c11c90e9
Dmitriy Lukin <lukin.dmitriy@gmail.com>
2023-01-09 01:53:59 +0500

    RAM balance - not need to switch off webserver now

--- main/WebServer.cpp
+++ main/WebServer.cpp
@@ -134,7 +134,7 @@ httpd_uri_t uri_put 				= { .uri = "/*"				, .method = HTTP_PUT		, .handler  = W
 httpd_uri_t uri_delete 				= { .uri = "/*"				, .method = HTTP_DELETE		, .handler  = WebServer_t::DELETEHandler, .user_ctx = NULL};
 
 void WebServer_t::HTTPStart() {
-	return;
+	//return;
 	ESP_LOGD(tag, "HTTPServer -> Start");
 
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
